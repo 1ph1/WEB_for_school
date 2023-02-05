020f63a85d6afd2e2efa325849e33fddbb9bdcbd
1ph1 <95160936+1ph1@users.noreply.github.com>
2023-02-05 19:54:10 +0700

    5 февраля

---
+++ lesson_18/script.js
@@ -0,0 +1,65 @@
+let stroke = "Hello world";
+console.log(typeof stroke);
+console.log(typeof 42);
+console.log(typeof true);
+
+console.log( `${stroke} я январь`);
+
+//let color = prompt("Какой твой любимый цвет?");
+//alert(`Пользователь ответил - ${color}`);
+
+//let a = prompt("Первое число", 1)
+//let b = prompt("Второе число", 2)
+//alert(+a + +b)
+
+//let height = prompt("высота")
+//let weight = prompt("вес")
+//let f1 = weight / (height**2)
+//alert(weight / (height**2))
+
+//let f = 1.72
+//let answ = "friend"
+//alert(`ИМТ больше у меня ${f1>f}, ИМТ больше у друга ${f>f1}`)
+
+//const name = "Иван"
+//const job = "учитель"
+//const firstJobYear = 2014
+//const today = 2023
+//console.log(`Прошу внимания, это ваш новый ${job}, ${name}. Его стаж работы ${today - firstJobYear}`)
+
+
+//console.log(`gthdfz cnhjrf
+//    gfgfgtdf 
+//    gfgfgfg
+//`)
+
+//let age = prompt("Введите ваш возраст")
+//if (age>18){alert("Рады вас видеть в нашем сообществе")}else{alert(`Пшел отсюда малолетка. Сможешь зайти через ${18 - age} лет`)}
+
+let a = prompt("Какой сегодня день недели?")
+console.log(a)
+switch (+a) {
+    case 1:
+        alert("Сегодня понедельник");
+        break;
+    case 2:
+        alert("вторник");
+        break;
+    case 3:
+        alert("среда");
+        break;
+    case 4:
+        alert("четверг");
+        break;
+    case 5:
+        alert("пятница");
+        break;
+    case 6:
+        alert("суббота");
+        break;
+    case 7:
+        ("воскресенье");
+        break;
+    default:
+        break;
+}
