0d68c6bfd78425c1cdfd6ef34e7d0f98436592f6
1ph1 <95160936+1ph1@users.noreply.github.com>
2023-05-14 19:58:32 +0700

    mamma mia!

---
+++ final_project/scripts.js
@@ -0,0 +1,49 @@
+const start = document.querySelector(".order_pizza");
+const Fpage = document.querySelector(".first_page");
+const Spage = document.querySelector(".second_page");
+
+const order = document.querySelector(".pizza_button");
+const adress = document.querySelector(".order_place");
+const overlay = document.querySelector(".overlay")
+
+const submit = document.querySelector(".submit")
+const city = document.querySelector(".city")
+const street = document.querySelector(".street")
+const building = document.querySelector(".building")
+
+start.addEventListener("click", function() {
+    Spage.classList.remove("hidden")
+    Fpage.classList.add("hidden")
+});
+
+order.addEventListener("click", function() {
+    adress.classList.remove("hidden")
+    overlay.classList.remove("hidden")
+})
+
+const closeModal = () => {
+    adress.classList.remove("hidden")
+};
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal()
+    }
+});
+
+
+submit.addEventListener("click", function() {
+    alert(`
+        Данные верны?
+        город ${city.value}, улица ${street.value}, дом ${building.value}
+
+        Если данные не верны, то заного введите данные 
+    `);
+    overlay.classList.add("hidden")
+    adress.classList.add("hidden")
+<<<<<<< HEAD
+    window.location.href = 'index2.html'
+=======
+
+>>>>>>> origin/main
+})
